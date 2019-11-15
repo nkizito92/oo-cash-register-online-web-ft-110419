07f1c9baefe5d10a5cf1f3444267b1469962761d
Kizito Njoku <nkizito57@gmail.com>
2019-11-15 21:37:08 +0000

    items def?

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
-
+require 'pry'
+class CashRegister
+   attr_accessor :total, :discount, :items
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end 
+  
+  def add_item(title,price, quantity=1)
+    @total += price*quantity
+  end
+  
+  def apply_discount 
+     @total -= (@total*(@discount/100.00)).to_i
+     @total != (@total*(@discount/100.00)).to_i ? "After the discount, the total comes to $#{self.total}." : "There is no discount to apply."
+     
+  end 
+  
+  def items
+    add_item.each do |produ|
+      @items.push(produ)
+    end 
+  end 
+end 
