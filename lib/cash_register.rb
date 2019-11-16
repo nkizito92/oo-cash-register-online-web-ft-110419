43c4fb9ca8a0b0dbbce4630ad61ea8270b378b40
Kizito Njoku <nkizito57@gmail.com>
2019-11-16 00:36:52 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,11 @@
 require 'pry'
 class CashRegister
-   attr_accessor :total, :discount, :items, :void_last_transaction, :priced
+   attr_accessor :total, :discount, :items, :void_last_transaction, :last_transaction
   def initialize(discount=0)
     @total = 0
     @discount = discount
     @items = []
-    @priced = []
+    @last_transaction = last_transaction
   end 
   
   def add_item(title,price, quantity=1)
@@ -13,22 +13,15 @@ class CashRegister
     quantity.times do 
       @items.push(title)
     end
-    quantity.times do 
-      @priced.push(price)
-    end
+    @last_transaction = price*quantity
   end 
   
   def apply_discount 
      @total -= (@total*(@discount/100.00)).to_i
      @total != (@total*(@discount/100.00)).to_i ? "After the discount, the total comes to $#{self.total}." : "There is no discount to apply."
   end 
-  def item 
-    @items
-  end 
-  
+ 
   def void_last_transaction
-  # find last item's prices 
-    @total -= @priced.last
-    #  return 0.0 if all items have been removed 
+    @total -= @last_transaction
   end 
 end 
